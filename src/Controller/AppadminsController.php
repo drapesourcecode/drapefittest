df3fb40d8964cd276724977a53d419d19c6f2352
drapesourcecode <52990960+drapesourcecode@users.noreply.github.com>
2019-08-26 11:22:45 +0530

    Update AppadminsController.php

--- src/Controller/AppadminsController.php
+++ src/Controller/AppadminsController.php
@@ -2658,12 +2658,8 @@ $merchantAuthentication = new AnetAPI\MerchantAuthenticationType();
 	  $request->setRefId($refId);
     $request->setTransactionRequest( $transactionRequestType);
     $controller = new AnetController\CreateTransactionController($request);
-    
-    if($getName=='drapefittest'){
-         $response = $controller->executeWithApiResponse(\net\authorize\api\constants\ANetEnvironment::SANDBOX);
-     }else if($getName=='drapefit'){
-         $response = $controller->executeWithApiResponse(\net\authorize\api\constants\ANetEnvironment::PRODUCTION);
-     }
+    $response = $controller->executeWithApiResponse(\net\authorize\api\constants\ANetEnvironment::SANDBOX);
+     
   
     
  ########################
