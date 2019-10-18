e6957b09535e5dafaf6e9977351b7d6955d98507
drapesourcecode <52990960+drapesourcecode@users.noreply.github.com>
2019-10-18 16:30:42 +0530

    18-10-2019

---
+++ vendor/PaymentTransactions/authorize-credit-card.php
@@ -0,0 +1,14 @@
+<?php
+
+require 'vendor/autoload.php';
+require_once 'constants/SampleCodeConstants.php';
+
+use net\authorize\api\contract\v1 as AnetAPI;
+use net\authorize\api\controller as AnetController;
+
+define("AUTHORIZENET_LOG_FILE", "phplog");
+
+
+
+
+?>
---
+++ vendor/PaymentTransactions/constants/SampleCodeConstants.php
@@ -0,0 +1,9 @@
+<?php
+class SampleCodeConstants
+{
+	//merchant credentials
+	const MERCHANT_LOGIN_ID = "5KWgbY6f2Q";
+	const MERCHANT_TRANSACTION_KEY = "2583R7tWxFW6n2Hq";
+}
+?>
+
